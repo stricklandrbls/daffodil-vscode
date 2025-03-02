4255946b7eee8401a2f51dab5dfa815bcbed93ec
Robert Strickland <strickland.rbls@gmail.com>
2025-03-01 23:21:02 -0600

    Migrated Svelte files to original path

--- src/svelte/webpack.config.js
+++ src/svelte/webpack.config.js
@@ -1,116 +1,53 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-//@ts-check
-'use strict'
-
-//@ts-check
-/** @typedef {import('webpack').Configuration} WebpackConfig **/
-
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const entry = path.resolve(__dirname, 'src', 'main.ts')
+const buildDir = path.resolve(__dirname, './dist')
+const svelteInclude = path.resolve('node_modules', 'svelte/src/runtime')
+const appHTML = path.resolve(__dirname, 'src/app.html')
+const indexHTML = path.resolve(buildDir, 'index.html')
 
 module.exports = (env) => /** @type WebpackConfig */ {
   const isDev = !!(env && env.development)
 
   return {
-    mode: 'none',
-    entry: {
-      index: path.resolve(__dirname, './src/views/dataEditor/index.ts'),
+    mode: isDev ? 'development' : 'production',
+    entry: entry,
+    output: {
+      filename: 'dataeditor.js',
+      path: buildDir,
     },
     resolve: {
       alias: {
-        svelte: path.resolve(__dirname, '../../', 'node_modules', 'svelte'),
-        src: path.resolve(__dirname, 'src'),
+        svelte: svelteInclude,
       },
-      extensions: ['.ts', '.mjs', '.js', '.css', '.svelte'],
-      mainFields: ['svelte', 'browser', 'module', 'main'],
-      conditionNames: ['svelte', 'browser', 'import'],
+      extensions: ['.mjs', '.js', '.ts', '.svelte', '.d.ts'],
+      conditionNames: ['browser', 'svelte'],
     },
     module: {
       rules: [
         {
-          test: /\.ts$/,
-          exclude: /node_modules/,
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: true,
-            configFile: path.resolve(__dirname, 'tsconfig.json'),
-          },
-        },
-        {
           test: /\.svelte$/,
-          loader: 'svelte-loader',
-          options: {
-            compilerOptions: {
-              dev: isDev,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              preprocess: require('svelte-preprocess')(),
             },
-
-            emitCss: true,
-            // @ts-ignore
-            preprocess: require('svelte-preprocess')({}),
-            onwarn: (_warning, _handler) => {}, // ignore warning mostly for stuff not being defined since they are passed in
           },
         },
         {
-          // required to prevent errors from Svelte on Webpack 5+, omit on Webpack 4
-          test: /node_modules\/svelte\/.*\.mjs$/,
-          resolve: {
-            fullySpecified: false,
-          },
-        },
-        {
-          test: /\.css$/,
-          sideEffects: true, // So we don't remove global css
-          use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
-            },
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: isDev,
-              },
-            },
-          ],
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
         },
       ],
     },
-
-    output: {
-      path: path.resolve(__dirname, '../../', 'dist', 'views', 'dataEditor'),
-      filename: 'index.js',
-      clean: true,
-      libraryTarget: 'commonjs2',
-    },
-
-    devtool: isDev ? 'source-map' : false,
-
     plugins: [
-      new MiniCssExtractPlugin({
-        filename: '../../styles.css',
-        chunkFilename: '../../styles.css',
+      new HtmlWebpackPlugin({
+        template: appHTML,
+        filename: indexHTML,
+        inject: 'body',
       }),
     ],
-    optimization: {
-      minimize: !isDev,
-      minimizer: ['...', new CssMinimizerPlugin()],
-      usedExports: true,
-    },
   }
 }
