4cd8e1e8284332a68c8b084fd94d3c66f8d1b5f6
Robert Strickland <strickland.rbls@gmail.com>
2025-03-04 17:53:10 -0600

    almost there

--- src/svelte/.kit/svelte.config.js
+++ src/svelte/.kit/svelte.config.js
@@ -1,18 +1,64 @@
-import adapter from '@sveltejs/adapter-static'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+// import adapter from '@sveltejs/adapter-static'
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   // Consult https://svelte.dev/docs/kit/integrations
+//   // for more information about preprocessors
+//   preprocess: vitePreprocess({ script: true }),
+//   compilerOptions: {
+//     dev: false,
+//     hydratable: true,
+//   },
+
+//   kit: {
+//     prerender: {
+//       entries: ['*'],
+//     },
+//     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+//     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+//     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+//     adapter: adapter({
+//       // default options are shown. On some platforms
+//       // these options are set automatically — see below
+//       pages: 'build',
+//       assets: 'build',
+//       fallback: undefined,
+//       precompress: false,
+//       strict: true,
+//     }),
+//   },
+// }
+
+// export default config
+
+const adapter = require('@sveltejs/adapter-static')
+const vitePreprocess = require('@sveltejs/vite-plugin-svelte')
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+module.exports = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess({ script: true }),
+  compilerOptions: {
+    dev: false,
+    hydratable: true,
+  },
 
   kit: {
+    prerender: {
+      entries: ['*'],
+    },
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    }),
   },
 }
-
-export default config
