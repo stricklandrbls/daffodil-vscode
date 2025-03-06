e4c6694a84294e497fd81dbafae4a55914326a9b
Robert Strickland <strickland.rbls@gmail.com>
2025-03-06 14:00:41 -0600

    Svelte5 builds statically using vite. Preview with 'serve'

--- src/svelte/src/components/Button.svelte
+++
@@ -1,5 +0,0 @@
-<script lang='ts'>
-    export let disableIf= false
-    export let name: string
-</script>
-<button disabled={disableIf}>{name}</button>
--- src/svelte/src/main.ts
+++ src/svelte/src/main.ts
@@ -1,21 +1,9 @@
-import { mount, SvelteComponent, type ComponentType } from 'svelte' // Causing Browser TypeErrors
-// import { createClassComponent } from 'svelte/legacy'
+import { mount } from 'svelte'
+import './app.css'
 import App from './App.svelte'
 
-/**
- * Svelte version < 4
- * - Requires DOM target
- */
-// const app = new App({
-//   target: document.getElementById('app')!,
-// })
-
-const app = mount(App as ComponentType, {
+const app = mount(App, {
   target: document.getElementById('app')!,
 })
 
-// const app = createClassComponent({
-//   component: App,
-//   target: document.getElementById('app')!,
-// })
 export default app
