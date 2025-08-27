4711629dabe91115cb1913f72c37751747dc1293
Robert Strickland <rstrickland@apache.org>
2025-08-27 16:29:50 -0500

    Data Editor Svelte 5 Upgrade w/ Vite Replacement

--- src/svelte/src/components/Debug/Debug.svelte.ts
+++ src/svelte/src/components/Debug/Debug.svelte.ts
@@ -14,25 +14,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
-{
-    "version": "2.0.0",
-    "tasks": [
-        {
-            "type": "npm",
-            "script": "compile",
-            "isBackground": false,
-            "group": {
-                "kind": "build",
-                "isDefault": true
-            },
-            "problemMatcher": "$tsc-watch"
-        },
-        {
-            "type": "npm",
-            "script": "watch",
-            "group": "build",
-            "isBackground": true,
-        }
-    ]
+import { getContext, setContext } from 'svelte'
+
+export interface DebugVariable {
+  id: string
+  valueStr: () => string
+}
+
+const debugVars = $state<DebugVariable[]>([])
+export const addVarToDebug = (...vars: DebugVariable[]) => {
+  debugVars.push(...vars)
+}
+
+export function setDebugVarContext() {
+  setContext('debug-vars', { add: addVarToDebug, get: () => debugVars })
+  return debugVars
+}
+export type DebugVarsContextType = {
+  add: typeof addVarToDebug
+}
+export function getDebugVarContext() {
+  return getContext('debug-vars') as DebugVarsContextType
 }
